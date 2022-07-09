46debc7968733281c861c26520ac948afa19e57f
Giuseppe Perrotta <102922764+Giuse03@users.noreply.github.com>
2022-07-09 15:32:13 +0200

    Update Utils.java

--- MainModule/src/main/java/net/giuse/mainmodule/utils/Utils.java
+++ MainModule/src/main/java/net/giuse/mainmodule/utils/Utils.java
@@ -4,10 +4,9 @@ import lombok.SneakyThrows;
 import net.giuse.mainmodule.messages.MessageActionbar;
 import net.giuse.mainmodule.messages.MessageChat;
 import net.giuse.mainmodule.messages.MessageTitle;
-import net.giuse.mainmodule.messages.interfaces.Message;
 import net.giuse.mainmodule.messages.interfaces.MessageLoader;
-import net.giuse.mainmodule.messages.type.MessageType;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
@@ -142,60 +141,66 @@ public class Utils {
      */
 
     public static void sendMessage(MessageLoader messageLoader, Player player, String idMessage, String... replace) {
-        List<Message> messages = messageLoader.getMessagesFromId(idMessage);
-        //Send Chat
-        for (Message message : messages) {
-            if (message.getMessageType().equals(MessageType.CHAT)) {
-                MessageChat messageChat = (MessageChat) message;
-                String messageToReplace = messageChat.getMessageChat();
-                if (replace.length == 0) {
-                    messageLoader.sendChat(player, Component.text(messageToReplace));
-                } else {
+        messageLoader.getMessagesFromId(idMessage).forEach(message -> {
+            switch (message.getMessageType()) {
+
+                //Send Action Bar
+                case ACTION_BAR:
+                    MessageActionbar messageActionbar = (MessageActionbar) message;
+                    String placeHolder = messageActionbar.getMessageBar();
+                    if (replace.length == 0) {
+                        messageLoader.sendActionBar(player, Component.text(placeHolder));
+                        break;
+                    }
+                    for (String args : replace) {
+                        String[] arg = args.split("=");
+                        placeHolder = placeHolder.replace(arg[0], arg[1]);
+                    }
+                    Component newComponentReplaced = Component.text(placeHolder);
+                    messageLoader.sendActionBar(player, newComponentReplaced);
+                    break;
+
+                //SEND CHAT
+                case CHAT:
+                    MessageChat messageChat = (MessageChat) message;
+                    String messageToReplace = messageChat.getMessageChat();
+                    if (replace.length == 0) {
+                        messageLoader.sendChat(player, Component.text(messageToReplace));
+                        break;
+                    }
                     String newComponent = null;
                     for (String args : replace) {
                         String[] arg = args.split("=");
                         newComponent = messageToReplace.replace(arg[0], arg[1]);
                     }
-                    messageLoader.sendChat(player, Component.text(newComponent));
-                }
+                    Component newComponentReplacedText = Component.text(newComponent);
+                    messageLoader.sendChat(player, newComponentReplacedText);
 
-            }
-
-            //Send Title
-            if (message.getMessageType().equals(MessageType.TITLE)) {
-                MessageTitle messageTitle = (MessageTitle) message;
-                String title = messageTitle.getTitle();
-                String subTitle = messageTitle.getSubTitle();
-                if (replace.length == 0) {
-                    messageLoader.sendTitle(player, Component.text(title), Component.text(subTitle), messageTitle.getFadeIn(), messageTitle.getStay(), messageTitle.getFadeOut());
-                } else {
+                    break;
+                //Send Title
+                case TITLE:
+                    MessageTitle messageTitle = (MessageTitle) message;
+                    String title = messageTitle.getTitle();
+                    String subTitle = messageTitle.getSubTitle();
+                    if (replace.length == 0) {
+                        TextComponent newTitleComponent = Component.text(title);
+                        TextComponent newSubTitleComponent = Component.text(subTitle);
+                        messageLoader.sendTitle(player, newTitleComponent, newSubTitleComponent, messageTitle.getFadeIn(), messageTitle.getStay(), messageTitle.getFadeOut());
+                        break;
+                    }
                     String newTitle = null, newSubTitle = null;
                     for (String args : replace) {
                         String[] arg = args.split("=");
                         newTitle = title.replace(arg[0], arg[1]);
                         newSubTitle = subTitle.replace(arg[0], arg[1]);
                     }
-                    messageLoader.sendTitle(player, Component.text(newTitle), Component.text(newSubTitle), messageTitle.getFadeIn(), messageTitle.getStay(), messageTitle.getFadeOut());
-                }
-
+                    TextComponent newTitleComponent = Component.text(newTitle);
+                    TextComponent newSubTitleComponent = Component.text(newSubTitle);
+                    messageLoader.sendTitle(player, newTitleComponent, newSubTitleComponent, messageTitle.getFadeIn(), messageTitle.getStay(), messageTitle.getFadeOut());
+                    break;
             }
 
-            //Send ActionBar
-            if (message.getMessageType().equals(MessageType.ACTION_BAR)) {
-                MessageActionbar messageActionbar = (MessageActionbar) message;
-                String placeHolder = messageActionbar.getMessageBar();
-
-                if (replace.length == 0) {
-                    messageLoader.sendActionBar(player, Component.text(placeHolder));
-                    return;
-                }
-                for (String args : replace) {
-                    String[] arg = args.split("=");
-                    placeHolder = placeHolder.replace(arg[0], arg[1]);
-                }
-                messageLoader.sendActionBar(player, Component.text(placeHolder));
-            }
-        }
+        });
     }
 
 }
